f396ec5aa373b886c84d29ed5ef31e0af9fe3209
Jaz-ma <potatoorgy3@gmail.com>
2022-06-06 21:34:16 +0100

    click events

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('khalatni vue :(')
+const app = Vue.createApp({
+data(){
+    return {
+        title:'The Final Empire',
+        author:'Brandon Sanderson',
+        age:'43'
+    }
+},
+methods:{
+    changeTitle(title){
+        this.title = title
+    }
+}
+})
+
+app.mount('#app')
