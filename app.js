94a59e36065d289fc6328dfb7cc70a3449b10676
Jaz-ma <potatoorgy3@gmail.com>
2022-06-09 19:41:47 +0100

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
 data(){
     return {
+    url:'https://www.google.com',
     showBooks:true,
     books:[
-        {title:'name of the wind ', author:'partick rothfuss', },
-        {title:'the way of the kings', author:'brandon something', },
-        {title:'the finale empire', author:'brandon something', },
+        {title:'name of the wind ', author:'partick rothfuss', img:'assets/1.jpg'},
+        {title:'the way of the kings', author:'brandon something', img:'assets/2.jpg'},
+        {title:'the finale empire', author:'brandon something', img:'assets/3.jpg'},
     ]
     }
 },
@@ -13,7 +14,7 @@ methods:{
     toggle(){
         this.showBooks = !this.showBooks
     },
-    
+
 }
 })
 
