dbae77b398b56caf3dd4c1a339ab6db75d2a92dc
Jaz-ma <potatoorgy3@gmail.com>
2022-06-09 17:44:02 +0100

    v-for lists

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const app = Vue.createApp({
 data(){
     return {
-        title:'The Final Empire',
-        author:'Brandon Sanderson',
-        age:'43',
-        showBooks: true,
-        x:0,
-        y:0,
-
+    showBooks:true,
+    books:[
+        {title:'name of the wind ', author:'partick rothfuss', },
+        {title:'the way of the kings', author:'brandon something', },
+        {title:'the finale empire', author:'brandon something', },
+    ]
     }
 },
 methods:{
     toggle(){
         this.showBooks = !this.showBooks
     },
-    handleEvent(e, data){
-        console.log(e,e.type)
-        if (data) {
-            console.log(data)
-        }
-    },
-    handleMouseMove(e){
-        this.x= e.offsetX
-        this.y= e.offsetY
-    }
+    
 }
 })
 
