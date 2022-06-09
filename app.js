8a424b0254d48b5d607e46a3cce6d49d51f3f5b2
Jaz-ma <potatoorgy3@gmail.com>
2022-06-09 17:35:15 +0100

    mouse events

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ data(){
         author:'Brandon Sanderson',
         age:'43',
         showBooks: true,
+        x:0,
+        y:0,
+
     }
 },
 methods:{
     toggle(){
         this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data){
+        console.log(e,e.type)
+        if (data) {
+            console.log(data)
+        }
+    },
+    handleMouseMove(e){
+        this.x= e.offsetX
+        this.y= e.offsetY
     }
 }
 })
