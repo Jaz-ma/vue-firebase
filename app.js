8abc9330de8edfebd288f8aab1850cd4f649831c
Jaz-ma <potatoorgy3@gmail.com>
2022-06-10 16:45:29 +0100

    toggling favs

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ data(){
     url:'https://www.google.com',
     showBooks:true,
     books:[
-        {title:'name of the wind ', author:'partick rothfuss', img:'assets/1.jpg'},
-        {title:'the way of the kings', author:'brandon something', img:'assets/2.jpg'},
-        {title:'the finale empire', author:'brandon something', img:'assets/3.jpg'},
+        {title:'name of the wind ', author:'partick rothfuss', img:'assets/1.jpg', isFav: true,},
+        {title:'the way of the kings', author:'brandon something', img:'assets/2.jpg',isFav: false,},
+        {title:'the finale empire', author:'brandon something', img:'assets/3.jpg',isFav:true,},
     ]
     }
 },
@@ -14,6 +14,9 @@ methods:{
     toggle(){
         this.showBooks = !this.showBooks
     },
+    toggleFav(book){
+        book.isFav=!book.isFav
+    }
 
 }
 })
